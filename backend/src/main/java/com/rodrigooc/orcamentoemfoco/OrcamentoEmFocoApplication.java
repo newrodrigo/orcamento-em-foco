63edc194181d10d4d7d074dfbddfbcd239426a0b
newrodrigo <rodrigocerqueira@outlook.com>
2023-10-31 21:07:55 -0300

    Project created

---
+++ backend/src/main/java/com/rodrigooc/orcamentoemfoco/OrcamentoEmFocoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigooc.orcamentoemfoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoEmFocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoEmFocoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rodrigooc/orcamentoemfoco/OrcamentoEmFocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigooc.orcamentoemfoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoEmFocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
